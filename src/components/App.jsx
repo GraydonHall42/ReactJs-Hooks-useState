5023a2c954e5ac05ddb450311e96c4575fdc648d
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-22 16:50:45 -0700

    ReactJS-state-variables

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App() {
-  return <div />;
+  //define initial state as 0
+  //count is the value of the state
+  //setcount is a function which can update the value of count
+  const [count, setCount] = React.useState(0);
+
+  //increases value of state variable by 1
+  function increase() {
+    setCount(count + 1);
+  }
+
+  //decreases value of state variable by 1
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
